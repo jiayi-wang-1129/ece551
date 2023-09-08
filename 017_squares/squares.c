65788185dbbd0e861e7fedc11cf13646510964d3
jw771 <jw771@duke.edu>
2023-09-08 01:15:43 -0400

    hw17

--- 017_squares/squares.c
+++ 017_squares/squares.c
@@ -20,24 +20,25 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
     for (int x = 0; x < w; x++) {
       //check if  EITHE
       if ((x >= x_offset && x < x_offset + size2) ||
-          (y >= y_offset && y < y_offset + size2 &&
-           (x = x_offset || x = x_offset + size2 - 1))) {
+          ((y >= y_offset && y < y_offset + size2) &&
+           (x = x_offset || x == x_offset + size2 - 1))) {
         printf("*");
       }
 
       else {
-        if ((x < size1 && (y = 0 || y = size1 - 1)) ||
-            (y < size1) && (x = 0 || x = size1 - 1)) {
+        if ((x < size1 && (y = 0 || y == size1 - 1)) ||
+            ((y < size1) && (x = 0 || x == size1 - 1))) {
           printf("#");
         }
         else {
           printf(" ");
         }
       }
+
+      printf("/n");
+      //if not,
+      // check if EITHER
+      //    x is less than size1 AND (y is either 0 or size1-1)
     }
-    printf("/n");
-    //if not,
-    // check if EITHER
-    //    x is less than size1 AND (y is either 0 or size1-1)
   }
 }
