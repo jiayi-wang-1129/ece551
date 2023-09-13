50320c88786bcf9ef57f740bf875867a7442a41e
jw771 <jw771@duke.edu>
2023-09-13 19:52:05 -0400

    h

---
+++ 028_tests_power/test-power.c
@@ -0,0 +1,19 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned power(unsigned x, unsigned y);
+void run_check(unsigned x, unsigned y, unsigned expected_ans) {
+  if (power(x, y) == expected_ans) {
+    printf("good");
+    exit(EXIT_SUCCESS);
+  }
+  else {
+    printf("Fail");
+    exit(EXIT_FAILURE);
+  }
+}
+int main(void) {
+  run_check(2, 2, 4);
+  return 0;
+}
