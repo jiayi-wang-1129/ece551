bf580ba9671fb0258188653825aa99ee1d8a3833
jw771 <jw771@duke.edu>
2023-09-13 23:37:13 -0400

    h

--- 028_tests_power/test-power.c
+++ 028_tests_power/test-power.c
@@ -6,15 +6,15 @@ unsigned power(unsigned x, unsigned y);
 void run_check(unsigned x, unsigned y, unsigned expected_ans) {
   if (power(x, y) == expected_ans) {
     printf("good");
-    exit(EXIT_SUCCESS);
   }
   else {
-    printf("Fail");
+    printf("F %d\n", power(x, y));
     exit(EXIT_FAILURE);
   }
 }
 int main(void) {
   run_check(2, 2, 4);
+  run_check(200, 4, 1600000000);
   run_check(0, 0, 1);
   return 0;
 }
