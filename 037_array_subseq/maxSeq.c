428ddd22ea32a3e6318ada1b745e22401a8022ca
jw771 <jw771@duke.edu>
2023-09-22 22:12:49 -0400

    h

--- 037_array_subseq/maxSeq.c
+++ 037_array_subseq/maxSeq.c
@@ -4,24 +4,24 @@
 size_t maxSeq(int * array, size_t n) {
   size_t count = 0;
   int array1[n];
-  array = &array[0];
+  int * p = &array1[1];
   for (size_t i = 1; i < n; i++) {
     if (array[i] > array[i - 1]) {
       count++;
       array1[i] = count;
-      array = &array1[i];
+      p = &array1[i];
     }
     else {
       count = 0;
     }
   }
-  if ((count == 0) & (n >= 1)) {
+  if ((*p == 0) & (n >= 1)) {
     return 1;
   }
   if ((count == 0) & (n < 1)) {
     return 0;
   }
   else {
-    return *array + 1;
+    return *p + 1;
   }
 }
