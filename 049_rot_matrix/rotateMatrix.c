648cba406aed14f1e1d414b1762f6558a31d146c
jw771 <jw771@duke.edu>
2023-10-04 13:57:34 -0400

    h

--- 049_rot_matrix/rotateMatrix.c
+++ 049_rot_matrix/rotateMatrix.c
@@ -3,34 +3,36 @@
 #include <stdlib.h>
 
 void rotateMatrix(FILE * f) {
-  int c;
-  char str[12] = {0};
-  //  char storing[10][10];
+  //  int c;
+  char line[12] = {0};
+  char storing[10][10];
   // const char* matrix={0};//pointer to read each line
+  int row = 0;
   //  const char ** ptr;//pointer to read inside of the line
-  //  while ((c = fgetc(f)) != EOF) {
-  //  int row = 0;
-  //  char * line = fgets(str, 12, f);
-  fprintf(stdout, "%s", fgets(str, 12, f));
-  // for (int i = 0; i < 10; i++) {
-  // storing[row][i] = *line;
-  // line++;
+  while (fgets(line, 12, f) != NULL) {
+    //  char * line = fgets(str, 12, f);
+    // fprintf(stdout, "%s", fgets(str, 12, f));
+    //  fgets(str, 12, f);
+    for (int i = 0; i < 10; i++) {
+      storing[row][i] = line[i];
+    }
+    row++;
+  }
+  //  for (int w = 0; w < 10; w++) {
+  //  for (int y = 0; y < 10; y++) {
+  //   fprintf(stdout, "%c", storing[w][y]);
   //  }
-  // row++;
+  // fprintf(stdout, "\n");
+  // }
+  //}
+
+  for (int n = 9; n >= 0; n--) {
+    for (int j = 0; j < 10; j++) {
+      fprintf(stdout, "%c", storing[j][n]);
+    }
+    fprintf(stdout, "\n");
+  }
 }
-//  for (int w = 0; w < 10; w++) {
-//  for (int y = 0; y < 11; y++) {
-//    fprintf(stdout, "%c", storing[w][y]);
-//  }
-// }
-
-// for (int n = 9; n >= 0; n--) {
-//  for (int j = 0; j < 10; j++) {
-//  fprintf(stdout, "%c", storing[j][n]);
-//  }
-//  fprintf(stdout, "\n");
-// }
-
 int main(int argc, char ** argv) {
   if (argc != 2) {
     fprintf(stderr, "invalid number of input");
