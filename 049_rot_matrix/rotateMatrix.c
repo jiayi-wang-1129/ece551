7540484dede87ee926625810e110d28f3667a354
jw771 <jw771@duke.edu>
2023-10-04 14:37:13 -0400

    h

--- 049_rot_matrix/rotateMatrix.c
+++ 049_rot_matrix/rotateMatrix.c
@@ -1,11 +1,13 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 void rotateMatrix(FILE * f) {
-  //  int c;
+  //
+
   char line[12] = {0};
   char storing[10][10];
+  //  char output[10][10];
   // const char* matrix={0};//pointer to read each line
   int row = 0;
   //  const char ** ptr;//pointer to read inside of the line
@@ -13,11 +15,27 @@ void rotateMatrix(FILE * f) {
     //  char * line = fgets(str, 12, f);
     // fprintf(stdout, "%s", fgets(str, 12, f));
     //  fgets(str, 12, f);
+
+    if (row == 10) {
+      fprintf(stderr, "Input has more than 10 lines");
+      EXIT_FAILURE;
+    }
+    if (strchr(line, '\n') != NULL) {
+      fprintf(stderr, "there is a line missing new space");
+      EXIT_FAILURE;
+    }
+    if (line[10] != '\n') {
+      fprintf(stderr, "input does not have right format");
+      EXIT_FAILURE;
+    }
+
     for (int i = 0; i < 10; i++) {
       storing[row][i] = line[i];
     }
     row++;
   }
+
+  //1, more than 10 lines 2.each line more than 10 string before /n, or has stuff after string
   //  for (int w = 0; w < 10; w++) {
   //  for (int y = 0; y < 10; y++) {
   //   fprintf(stdout, "%c", storing[w][y]);
@@ -29,10 +47,12 @@ void rotateMatrix(FILE * f) {
   for (int n = 9; n >= 0; n--) {
     for (int j = 0; j < 10; j++) {
       fprintf(stdout, "%c", storing[j][n]);
+      //  output[9 - n][j] = storing[j][n];
     }
     fprintf(stdout, "\n");
   }
 }
+
 int main(int argc, char ** argv) {
   if (argc != 2) {
     fprintf(stderr, "invalid number of input");
