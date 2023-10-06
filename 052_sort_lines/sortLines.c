1cbe58fe3f34cac4dfb299f1746956f28ed169d7
jw771 <jw771@duke.edu>
2023-10-06 10:53:40 -0400

    h

--- 052_sort_lines/sortLines.c
+++ 052_sort_lines/sortLines.c
@@ -1,7 +1,7 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 //This function is used to figure out the ordering of the strings
 //in qsort. You do not need to modify it.
 int stringOrder(const void * vp1, const void * vp2) {
@@ -13,8 +13,45 @@ int stringOrder(const void * vp1, const void * vp2) {
 void sortData(char ** data, size_t count) {
   qsort(data, count, sizeof(char *), stringOrder);
 }
+//data is the pointer to lines(strings),count is numer of lines,
+//sizeof(char *) is size of each line
 
 int main(int argc, char ** argv) {
+  char * line = NULL;
+  size_t * sz = 0;
+  if (argc == 1) {
+    FILE * f = fopen(argv[0], "r");
+    if (f == NULL) {
+      fprintf(stderr, "Could not open file");
+      return EXIT_FAILURE;
+    }
+    while (getline(&line, sz, f) != -1) {
+      for (size_t i = 0; i < *sz; i++) {
+        fprintf(stdout, "%s", line);
+      }
+      free(line);
+      free(sz);
+      line = NULL;
+    }
+
+    if (fclose(f) != 0) {
+      fprintf(stderr, "Could not close file");
+      return EXIT_FAILURE;
+    }
+  }
+  for (int i = 1; i < argc; i++) {
+    FILE * f = fopen(argv[i], "r");
+    if (f == NULL) {
+      fprintf(stderr, "Could not open file");
+      return EXIT_FAILURE;
+    }
+
+    if (fclose(f) != 0) {
+      fprintf(stderr, "Could not close file");
+      return EXIT_FAILURE;
+    }
+  }
+
   //WRITE YOUR CODE HERE!
 
   return EXIT_SUCCESS;
